fccb00f6377d979256a2e2908284ba848a2d6273
faresharmali <faresharmali3@gmail.com>
2022-06-13 11:49:47 +0200

    root removed

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ const CheckToken = (req, res, next) => {
   });
 };
 
-app.use("/",(req,res)=>{
-  res.status(200).json({message:"hello"})
-});
+
 app.use("/families", CheckToken, FamilyRouter);
 app.use("/users", UsersRouter);
 app.use("/informations", CheckToken, InformationsRouter);
