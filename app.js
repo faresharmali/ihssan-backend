72568337c46beed57f931053ccc562077db8a955
Fares Harmali <53355769+faresharmali@users.noreply.github.com>
2023-01-18 16:09:19 +0100

    fixed start command

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const CheckToken = (req, res, next) => {
   });
 };
 
+app.use("/",(req,res)=>{
+  res.send("Welcome to the API")
+} );
 app.use("/families", CheckToken, FamilyRouter);
 app.use("/users", UsersRouter);
 app.use("/informations", CheckToken, InformationsRouter);
